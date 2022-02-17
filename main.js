384ce2bc9f2b38209d2c45c274b710eabda28993
Naitiktiwa <86184694+Naitiktiwa@users.noreply.github.com>
2022-02-17 12:09:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,179 @@
+str= "";
+
+
+let ab = document.getElementById('b1').addEventListener('click',z)
+function z() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"1"}`
+    screen.value = str;
+
+    
+}
+
+let cd = document.getElementById('b2').addEventListener('click',b)
+function b() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"2"}`
+    screen.value = str;
+
+    
+}
+
+let ef = document.getElementById('b3').addEventListener('click',c)
+function c() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"3"}`
+    screen.value = str;
+
+    
+}
+
+let gh= document.getElementById('b4').addEventListener('click',d)
+function d() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"4"}`
+    screen.value = str;
+
+    
+}
+
+let ij = document.getElementById('b5').addEventListener('click',e)
+function e() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"5"}`
+    screen.value = str;
+
+    
+}
+
+let kl= document.getElementById('b6').addEventListener('click',f)
+function f() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"6"}`
+    screen.value = str;
+
+    
+}
+
+let mn = document.getElementById('b7').addEventListener('click',g)
+function g() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"7"}`
+    screen.value = str;
+
+    
+}
+
+let nas = document.getElementById('b8').addEventListener('click',h)
+function h() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"8"}`
+    screen.value = str;
+
+    
+}
+
+let qr = document.getElementById('b9').addEventListener('click',i)
+function i() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"9"}`
+    screen.value = str;
+
+    
+}
+
+let st = document.getElementById('b0').addEventListener('click',j)
+function j() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"0"}`
+    screen.value = str;
+
+    
+}
+
+let yz= document.getElementById('badd').addEventListener('click',t)
+function t() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"+"}`
+    screen.value = str;
+
+    
+}
+
+
+
+let wx= document.getElementById('bo').addEventListener('click',l)
+function l() {
+    
+    screen = document.getElementById('ol')
+    
+    str = eval(screen.value)
+    screen.value = str;
+
+    
+}
+let nati= document.getElementById('bc').addEventListener('click',kk)
+function kk() {
+    
+    screen = document.getElementById('ol')
+    str = ""
+    screen.value = ""
+    
+    
+
+    
+}
+let naa = document.getElementById('bmul').addEventListener('click',aa)
+function aa() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"*"}`
+    screen.value = str;
+
+    
+}
+
+let nbs = document.getElementById('bsub').addEventListener('click',bb)
+function bb() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"-"}`
+    screen.value = str;
+
+    
+}
+
+let ncs = document.getElementById('bdiv').addEventListener('click',cc)
+function cc() {
+    
+    screen = document.getElementById('ol')
+    
+    str += `${"/"}`
+    screen.value = str;
+
+    
+}
